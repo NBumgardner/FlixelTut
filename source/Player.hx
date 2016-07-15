273236eb5a44abd23df5035cf61681e4dff5f25f
Noah Bumgardner <noah@playgamecafe.com>
2016-07-15 13:22:58 -0500

    Completed the rest of section 4. Added movement animation to the Player class, with comments. Makes use of FlxObject and FlxSprite classes.

--- source/Player.hx
+++ source/Player.hx
@@ -1,6 +1,7 @@
 package;
 
 import flixel.FlxG;
+import flixel.FlxObject;
 import flixel.FlxSprite;
 import flixel.math.FlxPoint;
 import flixel.system.FlxAssets.FlxGraphicAsset;
@@ -18,12 +19,21 @@ class Player extends FlxSprite
 	public function new(?X:Float=0, ?Y:Float=0) 
 	{
 		super(X, Y);
-		makeGraphic(16, 16, FlxColor.BLUE);
+
+		// Add movement animations
+		loadGraphic(AssetPaths.player__png, true, 16, 16);
+		setFacingFlip(FlxObject.LEFT, false, false);
+		setFacingFlip(FlxObject.RIGHT, true, false);
+		animation.add("lr", [3, 4, 3, 5], 6, false);
+		animation.add("u", [6, 7, 6, 8], 6, false);
+		animation.add("d", [0, 1, 0, 2], 6, false);
+
 		drag.x = drag.y = 1600;
 	}
 
 	// Reads keyboard inputs to set the speed and angle of the Player.
 	//   Diagonal movement is slower than vertical or horizontal movement.
+	//   For animations, the Player faces UP or DOWN when moving diagonally.
 	private function movement():Void
 	{
 		var _up:Bool = false;
@@ -48,6 +58,7 @@ class Player extends FlxSprite
 		}
 
 		// If a valid move was made, calculate and set new movement speed and angle.
+		//   Set 'facing' for the correct animation, based on angle 'mA'.
 		if (_up || _down || _left || _right)
 		{
 			// Find angle of movement.
@@ -61,6 +72,7 @@ class Player extends FlxSprite
 					mA -= 45;
 				else if (_right)
 					mA += 45;
+				facing = FlxObject.UP; // the sprite should be facing UP
 			}
 			else if (_down)
 			{
@@ -70,16 +82,38 @@ class Player extends FlxSprite
 					mA += 45;
 				else if (_right)
 					mA -= 45;
+				facing = FlxObject.DOWN; // the sprite should be facing DOWN
 			}
 			else if (_left)
+			{
 				mA = 180;
+				facing = FlxObject.LEFT; // the sprite should be facing LEFT
+			}
 			else if (_right)
+			{
 				mA = 0;
+				facing = FlxObject.RIGHT; // the sprite should be facing RIGHT
+			}
 
 			// Set speed, then rotate it to angle mA.
 			//   Diagonal moves are slower.
 			velocity.set(speed, 0);
 			velocity.rotate(FlxPoint.weak(0, 0), mA);
+
+			// If Player is moving, velocity != 0 for an axis,
+			//   then change the animation to match Player facing.
+			if ((velocity.x != 0 || velocity.y != 0) && touching == FlxObject.NONE)
+			{
+				switch (facing) 
+				{
+					case FlxObject.LEFT, FlxObject.RIGHT:
+						animation.play("lr");
+					case FlxObject.UP:
+						animation.play("u");
+					case FlxObject.DOWN:
+						animation.play("d");
+				}
+			}
 		}
 	}
 

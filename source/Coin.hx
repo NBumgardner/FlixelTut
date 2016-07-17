d00c1788793fa1850653d3d451e0a6da8260df2a
Noah Bumgardner <noah@playgamecafe.com>
2016-07-17 17:30:45 -0500

    Completed section 8. Collected coins now fade out and up before being fully deleted.

--- source/Coin.hx
+++ source/Coin.hx
@@ -1,5 +1,7 @@
 package;
 import flixel.FlxSprite;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
 
 /**
  * Description:  Pick-up item for the player to collect by touching it.
@@ -11,5 +13,18 @@ class Coin extends FlxSprite
 	{
 		super(X, Y);
 		loadGraphic(AssetPaths.coin__png, false, 8, 8);
+	}
+
+	// When a coin is destroyed, it plays an animation to fade and move up.
+	override public function kill():Void
+	{
+		alive = false;
+		FlxTween.tween(this, { alpha: 0, y: y - 16 }, .33,
+			{ ease: FlxEase.circOut, onComplete: finishKill });
+	}
+
+	private function finishKill(_):Void
+	{
+		exists = false;
 	}
 }
